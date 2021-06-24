49503e7210cacd147cebc7c3558a556655e4e55b
Marcelina Sitko <marcelina.sitko@interia.pl>
2021-06-24 15:51:58 +0200

    reset

--- scripts/script.js
+++ scripts/script.js
@@ -6,9 +6,9 @@ const startBtn = document.getElementById("start");
 console.log(isRunning);
 const calcTime = () => {
   if (isRunning) {
-    start++;
     const minutes = Math.floor((start / 60) % 60);
     const seconds = Math.floor(start % 60);
+    start++;
     const time = (document.getElementById("time").innerHTML = `${timeFormat(
       minutes
     )}:${timeFormat(seconds)}`);
@@ -34,6 +34,15 @@ const timeFormat = (time) => {
   if (time < 10) time = `0${time}`;
   return time;
 };
+
+const resetTime = () => {
+  start = 0;
+  startBtn.innerHTML = "Stop";
+};
+
+document.getElementById("reset").addEventListener("click", () => {
+  resetTime();
+});
 
 setStart();
 setInterval(calcTime, 1000);
